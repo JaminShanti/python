cbe9d603c188b2fc6230bc97eed221ca43ad073c
Jamin Shanti <jamin_shanti@belk.com>
2018-10-02 10:21:40 -0400

    Updates to change default title

--- contact_bot/contact_bot.py
+++ contact_bot/contact_bot.py
@@ -6,15 +6,15 @@ import datetime
 from tabulate import tabulate
 import html2text
 
-
-email_titles = ['Checking up with you.']
-logSpace = '  '
+email_title_default = 'Checking up with you.'
 
 
 class ContactBot(object):
     def __init__(self):
         self.bot_file_exists = os.path.isfile('bot_file.yaml')
         self.verbose = True
+        self.logSpace = '  '
+        self.bother_bot_delay = 20
         if self.bot_file_exists:
             with open('bot_file.yaml', 'r') as f:
                 self.df = pd.io.json.json_normalize(yaml.load(f))
@@ -25,22 +25,22 @@ class ContactBot(object):
             with open('contact_config.yaml', 'r') as ymlfile:
                 self.cfg = yaml.safe_load(ymlfile)
         else:
-            print "WARN: no config file found."
+            self.logprint("no config file found.", "WARN")
 
     def logprint(self, msg, level):
         if self.verbose:
             print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-                  [:-3] + logSpace + level + logSpace + msg)
+                  [:-3] + self.logSpace + level + self.logSpace + msg)
         elif level != 'DEBUG':
             print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-                  [:-3] + logSpace + level + logSpace + msg)
+                  [:-3] + self.logSpace + level + self.logSpace + msg)
 
     def add(self):
         keep_adding = True
         sentence_list = []
         contact_type_choice = None
         print "Contact Types: "
-        for key,value in self.cfg['contact_types'].iteritems():
+        for key, value in self.cfg['contact_types'].iteritems():
             print key
         contact_type_choice = raw_input('Enter the contact type: ')
         while contact_type_choice not in self.cfg['contact_types']:
@@ -51,60 +51,66 @@ class ContactBot(object):
         first_name, last_name = full_name.split(' ')
         email_address = raw_input(
             'Enter the Contact\'s Email Address: ')
-        specialty=raw_input("What specialty for the %s? " % contact_type_choice)
+        specialty = raw_input("What specialty for the %s? " %
+                              contact_type_choice)
         while keep_adding:
-            special_sentence=raw_input(
+            special_sentence = raw_input(
                 'Enter a Familiar Sentence, enter blank to end: ')
             if special_sentence == '':
-                keep_adding=False
+                keep_adding = False
             else:
                 sentence_list.append(special_sentence)
         self.logprint("Pad extra sentence_list", "DEBUG")
         for i in range(len(sentence_list), 5):
             sentence_list.append('  ')
-        bother_bot_delay=14
-        last_contact_date=None
-        self.df=self.df.append({"full_name": full_name, "first_name": first_name,
+        last_contact_date = None
+        self.df = self.df.append({"full_name": full_name, "first_name": first_name,
                                   "last_name": last_name, "contact_type": contact_type_choice, "specialty": specialty,
                                   "email_address": email_address, "sentence_list": sentence_list,
-                                  "bother_bot_delay": bother_bot_delay, "last_contact_date": last_contact_date,
-                                  "email_template_type": contact_type['email_template']}, ignore_index = True)
+                                  "bother_bot_delay": self.bother_bot_delay, "last_contact_date": last_contact_date,
+                                  "email_template_type": contact_type['email_template']}, ignore_index=True)
 
     def display(self):
-        print tabulate(self.df[['full_name', 'contact_type', 'specialty']], headers = 'keys', tablefmt = 'psql')
+        print tabulate(self.df[['full_name', 'contact_type', 'specialty']], headers='keys', tablefmt='psql')
 
     def save(self):
         self.logprint("Saving bot_file.yaml!", "INFO")
         with open('bot_file.yaml', 'w') as file:
             yaml.dump(self.df.to_dict(orient='records'),
-                      file, default_flow_style = False)
+                      file, default_flow_style=False)
 
     def reload(self):
-        self.bot_file_exists=os.path.isfile('bot_file.yaml')
+        self.bot_file_exists = os.path.isfile('bot_file.yaml')
         if self.bot_file_exists:
             with open('bot_file.yaml', 'r') as f:
-                self.df=pd.io.json.json_normalize(yaml.load(f))
+                self.df = pd.io.json.json_normalize(yaml.load(f))
         else:
             print "Error: file bot_file.yaml not found"
 
     def send_single_email(self, row):
-        name=row['first_name'].encode("utf-8")
-        recipient=row['email_address'].encode("utf-8")
-        specialty=row['specialty'].encode("utf-8")
+        name = row['first_name'].encode("utf-8")
+        recipient = row['email_address'].encode("utf-8")
+        specialty = row['specialty'].encode("utf-8")
+        contact_type = self.cfg['contact_types'][row['contact_type']]
+        if contact_type['email_titles'].encode("utf-8") == '':
+            email_title = email_title_default
+        else:
+            email_title = contact_type['email_titles'].encode("utf-8")
         self.logprint("Opening File: %s" %
                       row['email_template_type'].encode("utf-8"), "INFO")
         with open(row['email_template_type'].encode("utf-8")) as f:
-            email_template=f.read()
+            email_template = f.read()
         self.logprint("first_name is %s, specialty is %s, sentence_list is %s" % (
             name, specialty, row['sentence_list']), "DEBUG")
-        html=email_template.format(first_name = name, specialty = specialty, special_sentence_1 = row['sentence_list'][0], special_sentence_2 = row['sentence_list'][
-                                     1], special_sentence_3 = row['sentence_list'][2], special_sentence_4 = row['sentence_list'][3], special_sentence_5 = row['sentence_list'][4])
+        html = email_template.format(first_name=name, specialty=specialty, special_sentence_1=row['sentence_list'][0], special_sentence_2=row['sentence_list'][
+            1], special_sentence_3=row['sentence_list'][2], special_sentence_4=row['sentence_list'][3], special_sentence_5=row['sentence_list'][4])
+        self.logprint("Email Subject: %s" % email_title, "DEBUG")
         self.logprint("Email Content: \n%s" %
                       html2text.html2text(html), "DEBUG")
-        yag=yagmail.SMTP(self.cfg['Email_Account'],
+        yag = yagmail.SMTP(self.cfg['Email_Account'],
                            self.cfg['Email_Password'])
-        yag.send(to = recipient,
-                 subject=email_titles[0], contents=html)
+        yag.send(to=recipient,
+                 subject=email_title, contents=html)
 
     def update_row(self, row):
         self.logprint("Reviewing Record: %s" % row['full_name'], "INFO")
---
+++ contact_bot/run_band_bot_cron.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append("..")
+from contact_bot import ContactBot
+band_bot = ContactBot()
+band_bot.run()
+print "Exiting..."
