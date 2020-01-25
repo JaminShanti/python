a571344e1f3c75bcb9c2743eac9b67a4fa15ef57
JaminShanti <radjammin@gmail.com>
2020-01-24 20:49:08 -0500

    adding date range

--- yt_channel_compare.py
+++ yt_channel_compare.py
@@ -6,6 +6,14 @@ import pandas as pd
 from os import path
 import time
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from pandas.plotting import register_matplotlib_converters
+import datetime as dt
+
+register_matplotlib_converters()
+
+last_many_days = 7
+today = dt.date.today()
 
 date = time.strftime("%Y-%m-%d")
 day_of_week = time.strftime("%A")
@@ -26,8 +34,6 @@ for key, value in channels.items():
     url = value
     page = requests.get(url, headers={
         'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}).text
-    # yt_inital_data =json.loads(re.findall(r'window\["ytInitialData"\] = (.*);', page)[0])
-    # total_views_today = yt_inital_data['contents']['twoColumnBrowseResultsRenderer']['tabs'][5]['tabRenderer']['content']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][0]['channelAboutFullMetadataRenderer']['viewCountText']['runs'][0]['text']
     viewCountText = json.loads(re.findall(r'\"viewCountText\":({.*views\"}]}),', page)[0])
     total_views_today = viewCountText['runs'][0]['text']
     total_views_today = int(total_views_today.replace(',', ''))
@@ -37,7 +43,7 @@ for key, value in channels.items():
 
 data = data.drop_duplicates(['channel_name','date'],keep='first')
 data.to_csv(csv_file_name, encoding='utf-8')
-data['date'] =  pd.to_datetime(data['date'])
+data['date'] = pd.to_datetime(data['date'])
 
 data = data.sort_values(by=['channel_name', 'date'])
 data['diff'] = data.groupby(['channel_name'])['total_views_today'].diff().fillna(0)
@@ -47,7 +53,16 @@ r = pd.date_range(start=data['date'].min(), end=data['date'].max())
 channels = data['channel_name'].unique()
 idx = pd.MultiIndex.from_product((r, channels), names=['date', 'channel_name'])
 data = data.set_index(['date', 'channel_name']).reindex(idx, fill_value=0).reset_index()
+data.set_index('date',inplace=True)
+data = data.loc[today - dt.timedelta(days=last_many_days):today]
+plt.style.use('dark_background')
+fig, ax = plt.subplots(figsize=(16, 5))
+for title, group in data.groupby('channel_name')['diff']:
+    ax.plot(group,label=title)
 
-data.set_index('date').groupby('channel_name')['diff'].plot(legend=True)
-plt.gcf().autofmt_xdate()
+myFmt = mdates.DateFormatter('%m-%d')
+ax.xaxis.set_major_formatter(myFmt)
+ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+fig.autofmt_xdate()
+plt.legend(bbox_to_anchor=(0.5, 1.10), ncol=4 , loc='upper center', borderaxespad=0.,fancybox=True, shadow=True)
 plt.savefig("yt_output\yt_channel_compare_%s.png" % date)
