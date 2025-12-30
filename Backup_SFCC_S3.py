38486d1549b9a0450e6db91312e414a9b5024f8e
Radjammin@gmail.com <my@eail.com>
2025-12-29 20:24:28 -0500

    Updating Code changes for the year.

--- Backup_SFCC_S3.py
+++ Backup_SFCC_S3.py
@@ -1,160 +1,204 @@
-##!/usr/bin/env python
-from datetime import datetime
-import os
-import socket
-from argparse import ArgumentParser
-import easywebdav
-import boto
-import boto.s3
-from boto.s3.key import Key
-import tempfile
-import shutil
+# python
+import argparse
+import logging
 import sys
+import tempfile
+from pathlib import Path
+from typing import Dict, List, Optional
+import xml.etree.ElementTree as ET
+import boto3
+import requests
+from requests.auth import HTTPBasicAuth
 
-__author__ = 'jshanti'
-__email__ = "devops@gmail.com"
-__status__ = "Development"
 
+class BackupSFCCInstance:
+    """
+    Backup SFCC instance files from WebDAV to S3 (modernized).
+    Usage example:
+      python Backup_SFCC_S3.py --webDavHostname webdav.example.com --webDavPath /path/ --webDAVuserID user \
+        --webDAVpassword pass --S3BucketName my-bucket --S3BucketPath backups/
+    """
 
-class BackupSFCCInstance(object):
-    def __init__(self):
-        self.hostname = socket.gethostname()
-        self.S3CurrentBackupList = []
-        self.instanceName = []
-        self.verbose = False
-        return
+    def __init__(self) -> None:
+        logging.basicConfig(level=logging.INFO, format="%(asctime)s  %(levelname)s  %(message)s")
+        self.logger = logging.getLogger("BackupSFCC")
+        self.webdav_hostname: str = ""
+        self.webdav_path: str = "/on/demandware.servlet/webdav/Sites/Impex/src/instance/"
+        self.webdav_user: Optional[str] = None
+        self.webdav_password: Optional[str] = None
+        self.webdav_filetype: str = "application/zip"
+        self.s3_bucket: str = ""
+        self.s3_prefix: str = ""
+        self.verbose: bool = False
+        self.s3_client = boto3.client("s3")
 
-    def percent_cb(self, complete, total):
-        sys.stdout.write('.')
-        sys.stdout.flush()
+    def handle_options(self) -> int:
+        parser = argparse.ArgumentParser(description="Backup SFCC WebDAV files to S3")
+        parser.add_argument("--webDavHostname", "-H", dest="webDavHostname", help="WebDAV host (host or https://host)")
+        parser.add_argument("--webDavPath", "-p", dest="webDavPath", default=self.webdav_path)
+        parser.add_argument("--webDavFileType", "-T", dest="webDavFileType", default=self.webdav_filetype)
+        parser.add_argument("--webDAVuserID", "-U", dest="webDAVuserID", default=None)
+        parser.add_argument("--webDAVpassword", "-P", dest="webDAVpassword", default=None)
+        parser.add_argument("--S3BucketName", "-B", dest="S3BucketName", required=True)
+        parser.add_argument("--S3BucketPath", "-d", dest="S3BucketPath", default="")
+        parser.add_argument("--verbose", action="store_true", dest="verbose", default=False)
 
-    def sizeof_fmt(self, num, suffix='B'):
-        for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-            if abs(num) < 1024.0:
-                return "%3.1f%s%s" % (num, unit, suffix)
-            num /= 1024.0
-        return "%.1f%s%s" % (num, 'Yi', suffix)
-
-    def upload_file_to_S3_bucket(self, uploadFile):
-        print 'Uploading %s to Amazon S3 bucket %s' % \
-              (uploadFile, self.S3BucketName)
-        k = Key(self.s3Bucket)
-        k.key = self.S3BucketPath + uploadFile
-        k.set_contents_from_filename(self.dirpath + '/' + uploadFile,
-                                     cb=self.percent_cb, num_cb=10)
-        return retcode, errormsgs
-
-    def handle_options(self):
-        retcode = 0
-        errormsgs = []
-        parser = ArgumentParser()
-
-        group = parser.add_argument_group('group')
-
-        group.add_argument("--webDavHostname", '-H', dest="webDavHostname", action="store",
-                           help="webDavLocation, default=None")
-        group.add_argument("--webDavPath", '-p', dest="webDavPath", action="store",
-                           default='on/demandware.servlet/webdav/Sites/Impex/src/instance/',
-                           help="webDavPath, default=None")
-        group.add_argument("--webDavFileType", '-T', dest="webDavFileType", action="store", default='application/zip',
-                           help="webDavFileType, default=application/zip")
-        group.add_argument("--webDAVuserID", '-U', dest="webDAVuserID", action="store", default="backup",
-                           help="userID for webDav Mount, default=backup")
-        group.add_argument("--webDAVpassword", '-P', dest="webDAVpassword", action="store",
-                           help="Password for webDAVuserID, default=None")
-        group.add_argument("--S3BucketName", '-B', dest="S3BucketName", action="store",
-                           help="S3 BucketName, default=None")
-        group.add_argument("--S3BucketPath", '-d', dest="S3BucketPath", action="store",
-                           help="Destination path for backups, default=webDavHostname/backups/")
-        group.add_argument("--verbose", dest="verbose", action="store_true", default=False,
-                           help="Be verbose, default=False")
-
-        options = parser.parse_args()
-
-        self.verbose = options.verbose
-        self.logprint("Argument provided: verbose '%s'" % (self.verbose), "INFO")
-        self.webDavHostname = options.webDavHostname
-        self.logprint("Argument provided: webDavHostname '%s'" % (self.webDavHostname), "INFO")
-        self.webDavPath = options.webDavPath
-        self.logprint("Argument provided: webDavPath '%s'" % (self.webDavPath), "INFO")
-        self.webDavFileType = options.webDavFileType
-        self.logprint("Argument provided: webDavFileType '%s'" % (self.webDavFileType), "INFO")
-        self.webDAVuserID = options.webDAVuserID
-        self.logprint("Argument provided: webDAVuserID '%s'" % (self.webDAVuserID), "INFO")
-        self.webDAVpassword = options.webDAVpassword
-        self.logprint("Argument provided: webDAVpassword '.....'", "INFO")
-        self.S3BucketName = options.S3BucketName
-        self.logprint("Argument provided: S3Bucket '%s'" % (self.S3BucketName), "INFO")
-        self.S3BucketPath = options.S3BucketPath
-        self.logprint("Argument provided: S3BucketPath '%s'" % (self.S3BucketPath), "INFO")
-        self.logprint("%s%d, %s%s" % ("Retcode=", retcode, "ErrorMessages=", errormsgs), "INFO")
-        return retcode, errormsgs
-
-    def connectS3_and_getCurrentBackupList(self):
-        self.logprint("Connecting to AWS S3...", "INFO")
-        self.s3 = boto.connect_s3()
-        self.logprint("Connecting to S3Location: '%s:%s'" % (self.S3BucketName, self.S3BucketPath), 'INFO')
-        self.s3Bucket = self.s3.get_bucket(self.S3BucketName)
-        self.s3CurrentBackupObject = self.s3Bucket.list(prefix=self.S3BucketPath, delimiter='/')
-        for backedupFile in self.s3CurrentBackupObject:
-            backupFilenameOnly = backedupFile.name.rsplit('/', 1)[1]
-            self.S3CurrentBackupList.append(backupFilenameOnly)
-        self.logprint("The Current File Count is : '%s'" % (len(self.S3CurrentBackupList)), 'INFO')
-        self.logprint("The S3CurrentBackupList is '%s'" % (self.S3CurrentBackupList), 'DEBUG')
-        return retcode, errormsgs
-
-    def connectWebDAV_and_Backupfiles_toS3(self):
-        self.logprint("Connecting to WebDAV Instance '%s'" % self.webDavHostname, "INFO")
-        self.webdav = easywebdav.connect(self.webDavHostname,
-                                         username=self.webDAVuserID,
-                                         password=self.webDAVpassword,
-                                         protocol='https')
-        self.logprint("Inspecting File Path:  '%s'" % self.webDavPath, "INFO")
-        self.returnFileList = self.webdav.ls(self.webDavPath)
-        self.logprint("Locating FileType:  '%s'" % self.webDavFileType, "INFO")
-        self.returnFileListSelected = [x for x in self.returnFileList if self.webDavFileType in x.contenttype]
-        self.dirpath = tempfile.mkdtemp()
-        self.logprint("Creating Directory '%s'" % (self.dirpath), 'DEBUG')
-        for webdavfile in self.returnFileListSelected:
-            webdavfileName = webdavfile.name.rsplit('/', 1)[1]
-            self.logprint("Discovered: '%s' size '%s' Modified_Time '%s' '" % (
-                webdavfileName, self.sizeof_fmt(webdavfile.size), webdavfile.mtime), 'INFO')
-            if webdavfileName in self.S3CurrentBackupList:
-                self.logprint("Is Present in S3 as '%s:%s%s'" % (self.S3BucketName, self.S3BucketPath, webdavfileName),
-                              'INFO')
-            else:
-                self.logprint("'%s' Backup is New!!" % (webdavfileName), 'INFO')
-                self.logprint("saving to %s/%s" % (self.dirpath, webdavfileName), 'INFO')
-                self.webdav.download(self.webDavPath + webdavfileName,
-                                     self.dirpath + "/" + webdavfileName)
-                self.logprint("Uploading as '%s:%s%s'" % (self.S3BucketName, self.S3BucketPath, webdavfileName), 'INFO')
-                self.upload_file_to_S3_bucket(webdavfileName)
-                self.logprint("Removing to %s/%s" % (self.dirpath, webdavfileName), 'INFO')
-                os.remove(self.dirpath + '/' + webdavfileName)
-        self.logprint("Removing Directory '%s'" % (self.dirpath), 'DEBUG')
-        shutil.rmtree(self.dirpath)
-
-        return retcode, errormsgs
-
-    def logprint(self, msg, level):
+        opts = parser.parse_args()
+        self.verbose = opts.verbose
         if self.verbose:
-            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] + logSpace + level + logSpace + msg)
-        elif level != 'DEBUG':
-            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] + logSpace + level + logSpace + msg)
+            self.logger.setLevel(logging.DEBUG)
+
+        self.webdav_hostname = opts.webDavHostname or ""
+        self.webdav_path = opts.webDavPath
+        self.webdav_filetype = opts.webDavFileType
+        self.webdav_user = opts.webDAVuserID
+        self.webdav_password = opts.webDAVpassword
+        self.s3_bucket = opts.S3BucketName
+        self.s3_prefix = opts.S3BucketPath or ""
+        # normalize prefix to end with '/' or be empty
+        if self.s3_prefix and not self.s3_prefix.endswith("/"):
+            self.s3_prefix += "/"
+
+        self.logger.debug("Options: webdav=%s path=%s filetype=%s s3=%s prefix=%s",
+                          self.webdav_hostname, self.webdav_path, self.webdav_filetype,
+                          self.s3_bucket, self.s3_prefix)
+        return 0
+
+    def _list_s3_objects(self) -> List[str]:
+        """Return list of filenames already present in S3 under prefix (basename only)."""
+        paginator = self.s3_client.get_paginator("list_objects_v2")
+        page_iterator = paginator.paginate(Bucket=self.s3_bucket, Prefix=self.s3_prefix)
+        existing = []
+        for page in page_iterator:
+            for obj in page.get("Contents", []):
+                key = obj["Key"]
+                existing.append(Path(key).name)
+        self.logger.debug("S3 existing files: %s", existing)
+        return existing
+
+    def _propfind_list(self) -> List[Dict]:
+        """
+        Perform a PROPFIND to list entries in the WebDAV path.
+        Returns list of dictionaries with keys: name, href, contenttype, size, modified.
+        """
+        # build base URL
+        base = self.webdav_hostname
+        if not base.startswith("http://") and not base.startswith("https://"):
+            base = "https://" + base
+        # ensure path formatting
+        webdav_url = base.rstrip("/") + "/" + self.webdav_path.lstrip("/")
+        headers = {"Depth": "1"}
+        body = """<?xml version="1.0" encoding="utf-8" ?>
+<D:propfind xmlns:D="DAV:">
+  <D:prop>
+    <D:resourcetype/>
+    <D:getcontenttype/>
+    <D:getcontentlength/>
+    <D:getlastmodified/>
+  </D:prop>
+</D:propfind>"""
+        auth = HTTPBasicAuth(self.webdav_user, self.webdav_password) if self.webdav_user else None
+        resp = requests.request("PROPFIND", webdav_url, data=body, headers=headers, auth=auth, timeout=30)
+        resp.raise_for_status()
+        # parse XML response
+        ns = {"D": "DAV:"}
+        root = ET.fromstring(resp.content)
+        items = []
+        for response in root.findall("D:response", ns):
+            href = response.find("D:href", ns)
+            if href is None:
+                continue
+            href_text = href.text or ""
+            prop = response.find("D:propstat/D:prop", ns)
+            if prop is None:
+                continue
+            ct = prop.find("D:getcontenttype", ns)
+            size = prop.find("D:getcontentlength", ns)
+            lastmod = prop.find("D:getlastmodified", ns)
+            name = Path(href_text).name
+            # skip collection entries that represent the folder itself
+            if not name:
+                continue
+            items.append({
+                "name": name,
+                "href": href_text,
+                "contenttype": ct.text if ct is not None else "",
+                "size": int(size.text) if size is not None and size.text and size.text.isdigit() else None,
+                "modified": lastmod.text if lastmod is not None else None
+            })
+        self.logger.debug("WebDAV items: %s", items)
+        return items
+
+    def _download_file(self, href: str, dest_path: Path) -> None:
+        """Download a file from WebDAV href to local dest_path."""
+        # If href is relative, build full URL
+        if href.startswith("/"):
+            base = self.webdav_hostname
+            if not base.startswith("http://") and not base.startswith("https://"):
+                base = "https://" + base
+            url = base.rstrip("/") + href
+        else:
+            url = href if href.startswith("http") else self.webdav_hostname.rstrip("/") + "/" + href.lstrip("/")
+        auth = HTTPBasicAuth(self.webdav_user, self.webdav_password) if self.webdav_user else None
+        with requests.get(url, stream=True, auth=auth, timeout=60) as r:
+            r.raise_for_status()
+            with open(dest_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+        self.logger.info("Downloaded %s -> %s", url, dest_path)
+
+    class _Progress:
+        def __init__(self, logger: logging.Logger):
+            self._seen = 0
+            self._logger = logger
+
+        def __call__(self, bytes_amount):
+            # simple dot progress to stdout (keeps output compact)
+            self._seen += bytes_amount
+            sys.stdout.write(".")
+            sys.stdout.flush()
+
+    def _upload_to_s3(self, local_path: Path, s3_key: str) -> None:
+        cb = self._Progress(self.logger)
+        extra_args = {}
+        # let boto3 infer content-type or set explicitly if desired
+        self.s3_client.upload_file(str(local_path), self.s3_bucket, s3_key, Callback=cb, ExtraArgs=extra_args)
+        sys.stdout.write("\n")
+        self.logger.info("Uploaded to s3://%s/%s", self.s3_bucket, s3_key)
+
+    def run(self) -> int:
+        try:
+            self.handle_options()
+            self.logger.info("Starting backup run")
+            existing = set(self._list_s3_objects())
+            webdav_items = self._propfind_list()
+            to_process = [i for i in webdav_items if self.webdav_filetype in (i.get("contenttype") or "")]
+            if not to_process:
+                self.logger.info("No WebDAV files matching type %s found.", self.webdav_filetype)
+                return 0
+            with tempfile.TemporaryDirectory() as tmpdir:
+                tmpdir_path = Path(tmpdir)
+                for item in to_process:
+                    name = item["name"]
+                    if name in existing:
+                        self.logger.info("%s already present in S3, skipping", name)
+                        continue
+                    dest = tmpdir_path / name
+                    self._download_file(item["href"], dest)
+                    s3_key = f"{self.s3_prefix}{name}"
+                    self._upload_to_s3(dest, s3_key)
+                    dest.unlink(missing_ok=True)
+            self.logger.info("Backup run complete")
+            return 0
+        except requests.HTTPError as e:
+            self.logger.error("HTTP error: %s", e)
+            return 2
+        except Exception as e:
+            self.logger.exception("Unhandled error: %s", e)
+            return 1
 
 
-if __name__ == '__main__':
-    logSpace = '  '
-    print(
-    datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] + logSpace + 'INFO' + logSpace + os.path.basename(__file__))
-    retcode = 0
-    errormsgs = []
-    currentDirectory = os.getcwd()
-    backupSFCCInstance = BackupSFCCInstance()
-
-    tmpRetcode, tmpErrorMsgs = backupSFCCInstance.handle_options()
-    tmpRetcode, tmpErrorMsgs = backupSFCCInstance.connectS3_and_getCurrentBackupList()
-    tmpRetcode, tmpErrorMsgs = backupSFCCInstance.connectWebDAV_and_Backupfiles_toS3()
-
-    retcode += tmpRetcode
-    errormsgs += tmpErrorMsgs
+if __name__ == "__main__":
+    rc = BackupSFCCInstance().run()
+    sys.exit(rc)
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# python
-Python is core to understanding cross platform scripting in today's cloud and on-premise infrastructure.  In today's industry Python has become the most popular scripting language available.  Weather extending core language functionalty or automating product tasks, python fills in the potholes.
+![Profile Image](assets/profile.jpg)
+
+> Welcome to my retro for the `python` repo (master).
+
+Profile: [https://www.linkedin.com/in/jamin-shanti](https://www.linkedin.com/in/jamin-shanti)  
+Repo: `git@github.com:JaminShanti/python.git` (branch: `master`)
+
+--- 
+
+## About Me
+- Software developer focused on Python projects and tooling.
+- This project generates git activity plots (authors, filenames, hour/day distributions).
+- For full professional details see the LinkedIn profile above.
+
+## Top 8
+1. Coding
+2. Open source
+3. Data analysis
+4. Automation
+5. Visualizations
+6. Coffee
+7. Retro UI vibes
+8. Shipping features
+
+## Friends
+- `origin` — `git@github.com:JaminShanti/python.git`
+- Local branch: `master`
+
+## Comments
+"Cool plots!" — fans welcome PRs and issues.
+
+## How to Run
+Prerequisites:
+- Windows, Python 3.13+
+- `git` on PATH
+- Python packages: `pandas`, `matplotlib`, `gitpython`
+
+Install:
+```powershell
+pip install pandas matplotlib gitpython
