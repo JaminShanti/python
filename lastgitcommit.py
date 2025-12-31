7ad05d8ccf3935d097385d8487fea917dea5fb34
Radjammin@gmail.com <my@eail.com>
2025-12-31 17:39:32 -0500

    Updating Code changes for the year.

--- lastgitcommit.py
+++ lastgitcommit.py
@@ -1,3 +1,4 @@
+# python
 import git
 from io import StringIO
 from argparse import ArgumentParser
@@ -5,9 +6,10 @@ from datetime import datetime
 from collections import OrderedDict
 import sys
 import warnings
+import logging
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-    warnings.filterwarnings('ignore', message="numpy.ufunc size changed")
+    warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
     import pandas as pd
 
 
@@ -17,24 +19,16 @@ __creation_date__ = "Jul 23th 2018"
 
 secret_git_user = 'Rumpelstiltskin'
 
-
-def logprint(msg, level):
-    if verbose:
-        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-              [:-3] + logSpace + level + logSpace + msg)
-    elif level != 'DEBUG':
-        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-              [:-3] + logSpace + level + logSpace + msg)
+# module logger
+logger = logging.getLogger("lastgitcommit")
 
 
 def get_git_log(_repoFilePath='.'):
     repo = git.Repo(_repoFilePath)
     git_repo = repo.git
     repo_name = repo.remotes[0].url
-    git_log_output = git_repo.log('--since=last month',
-                                  '--numstat', '--pretty=format:\t\t\t%h\t%ad\t%aN\t%ae')
-    git_log_output = git_repo.log(
-        '--numstat', '--no-merges', '--pretty=format:\t\t\t%h\t%ad\t%aN\t%ae')
+    # use numstat and a stable pretty format; original used tab-separated fields parsed by pandas
+    git_log_output = git_repo.log('--numstat', '--no-merges', '--pretty=format:\t\t\t%h\t%ad\t%aN\t%ae')
     git_log = pd.read_csv(StringIO(git_log_output),
                           sep="\t",
                           header=None,
@@ -57,6 +51,7 @@ def handle_options(parser):
     global to_json
     global last_build_user
     global find_special_file
+
     group.add_argument("--gitPath", '-g', dest="git_path", action="store", default=".",
                        help="/Path/to/git/repo, default is CurrentDirectory.")
     group.add_argument("--authorEmailAddress", '-e', dest="include_author_email_address",
@@ -76,26 +71,27 @@ def handle_options(parser):
 
     options = parser.parse_args()
     verbose = options.verbose
-    logprint("Argument provided: verbose '%s'" % (verbose), "DEBUG")
+
+    # configure logging: mirror original timestamp+level style
+    log_level = logging.DEBUG if verbose else logging.INFO
+    logging.basicConfig(level=log_level, format="%(asctime)s  %(levelname)s  %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+    logger.setLevel(log_level)
+
+    logger.debug("Argument provided: verbose '%s'", verbose)
     git_path = options.git_path
-    logprint("Argument provided: git_path '%s'" % (git_path), "DEBUG")
+    logger.debug("Argument provided: git_path '%s'", git_path)
     include_author_email_address = options.include_author_email_address
-    logprint("Argument provided: include_author_email_address '%s'" %
-             (include_author_email_address), "DEBUG")
+    logger.debug("Argument provided: include_author_email_address '%s'", include_author_email_address)
     include_author_name = options.include_author_name
-    logprint("Argument provided: include_author_name '%s'" %
-             (include_author_name), "DEBUG")
+    logger.debug("Argument provided: include_author_name '%s'", include_author_name)
     include_filelist = options.include_filelist
-    logprint("Argument provided: include_filelist '%s'" %
-             (include_filelist), "DEBUG")
+    logger.debug("Argument provided: include_filelist '%s'", include_filelist)
     to_json = options.to_json
-    logprint("Argument provided: to_json '%s'" % (to_json), "DEBUG")
+    logger.debug("Argument provided: to_json '%s'", to_json)
     last_build_user = options.last_build_user
-    logprint("Argument provided: last_build_user '%s'" %
-             (last_build_user), "DEBUG")
+    logger.debug("Argument provided: last_build_user '%s'", last_build_user)
     find_special_file = options.find_special_file
-    logprint("Argument provided: find_special_file '%s'" %
-             (find_special_file), "DEBUG")
+    logger.debug("Argument provided: find_special_file '%s'", find_special_file)
 
 
 def return_ouput():
@@ -110,39 +106,38 @@ def return_ouput():
                 if last_build_user in last_commit.author.values:
                     break
         else:
-            logprint("last_build_user: %s not present in my_git_log" %
-                     last_build_user, "DEBUG")
+            logger.debug("last_build_user: %s not present in my_git_log", last_build_user)
             if last_build_user != secret_git_user:
-                logprint("Fatal: User: %s not present in Repository!" %
-                         last_build_user, "FATAL")
+                logger.fatal("User: %s not present in Repository!", last_build_user)
                 sys.exit(1)
     sha_commits = last_commit['sha'].tolist()
     sha_commits = OrderedDict((x, True) for x in sha_commits).keys()
-    logprint("the current length of sha_commits is %s" %
-             len(sha_commits), "DEBUG")
-    for i in reversed(sha_commits):
-        logprint("current sha is %s" % i, "DEBUG")
+    logger.debug("the current length of sha_commits is %s", len(list(sha_commits)))
+    for i in reversed(list(sha_commits)):
+        logger.debug("current sha is %s", i)
+        row = last_commit[last_commit.sha == i]
         if include_author_email_address:
-            print last_commit[last_commit.sha == i].email_address.values[0]
+            print(str(row.email_address.values[0]))
         if include_author_name:
-            print last_commit[last_commit.sha == i].author.values[0]
+            print(str(row.author.values[0]))
         if include_filelist:
-            print last_commit[last_commit.sha == i].filename.tolist()
+            print(row.filename.tolist())
         if to_json:
-            print last_commit[last_commit.sha == i].to_json()
+            print(row.to_json())
         if verbose:
-            print last_commit[last_commit.sha == i].timestamp.values[0]
-        if last_commit[last_commit.sha == i].author.values[0] != last_build_user and find_special_file:
-            if [s for s in last_commit[last_commit.sha == i].filename.tolist() if find_special_file in s]:
-                logprint("FileName=%s file modified by non-Build User=%s" %
-                         (find_special_file,last_commit[last_commit.sha == i].author.values[0]), "WARNING")
+            # timestamp may be numpy datetime or string
+            ts = row.timestamp.values[0]
+            print(str(ts))
+        if row.author.values[0] != last_build_user and find_special_file:
+            if [s for s in row.filename.tolist() if find_special_file in s]:
+                logger.warning("FileName=%s file modified by non-Build User=%s",
+                               find_special_file, row.author.values[0])
 
 
 if __name__ == '__main__':
-    logSpace = '  '
     handle_options(ArgumentParser())
     my_git_log = get_git_log(git_path)
     if len(my_git_log) == 0:
-        print "No Recent Commits Found, Exiting..."
+        print("No Recent Commits Found, Exiting...")
         sys.exit(1)
     return_ouput()
